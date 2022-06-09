cb4280f88932f402c9872d5a3aa5c3ce64f95e1b
Curse-Ka <wkrska@bu.edu>
2022-06-09 16:14:02 -0400

    testbenches for auto_incr_vec, sync_FIFO, and ISA_decoder

--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_ISA_decoder.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_ISA_decoder.sv
@@ -1,26 +1,105 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/08/2022 09:43:09 AM
-// Design Name: 
-// Module Name: test_
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+`ifndef MY_INTERFACE
+  `define MY_INTERFACE
+  `include "my_interface.vh"
+`endif
 
-module test_ISA_decoder(
+module test_ISA_decoder;
+    // **dwidth_inst = 32**
+    reg [dwidth_inst-1:0] instr; // vector instruction
+    reg clk;
+    reg rst;
+    wire ctrl_i_mux2_tvalid;
+    wire [4:0] rs1; // source register 1
+    wire [4:0] rs2; // source register 2
+    wire [4:0] rd; // dest register
+    wire [dwidth_RFadd-1:0] ITR;
+    wire wen_ITR;
+    wire is_vle32_vv;
+    wire is_vse32_vv;
+    wire is_vmacc_vv;
+    wire is_vmv_vi;
+    wire is_vstreamout;
+    wire is_beq;
+    wire is_csr;
+    wire [dwidth_RFadd-1:0] vr_addr; // vector register file
+    wire [dwidth_RFadd-1:0] vw_addr; // vector register file
+    wire is_not_vect; // used to inform us about stall (if it is zero we should stall)
+    wire [11:0] branch_immediate;
+    wire [dwidth_int-1:0] R_immediate;
+    wire [2:0] op;
+    wire [2:0] op_scalar;
+    wire wen_RF_scalar;
+    
+    ISA_decoder ISA_decoder_inst(
+        .instr(instr),
+        .clk(clk),
+        .rst(rst),
+        .ctrl_i_mux2_tvalid(ctrl_i_mux2_tvalid),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .ITR(ITR),
+        .wen_ITR(wen_ITR),
+        .vr_addr(vr_addr),
+        .vw_addr(vw_addr),
+        .is_not_vect(is_not_vect),
+        .is_vle32_vv(is_vle32_vv),
+        .is_vse32_vv(is_vse32_vv),
+        .is_vmacc_vv(is_vmacc_vv),
+        .is_vmv_vi(is_vmv_vi),
+        .is_beq(is_beq),
+        .is_csr(is_csr),
+        .branch_immediate(branch_immediate),
+        .R_immediate(R_immediate),
+        .op(op),
+        .op_scalar(op_scalar),
+        .wen_RF_scalar(wen_RF_scalar)
+        );
+    always begin
+        clk = 1;
+        #5;
+        clk = 0;
+        #5;
+    end
 
-    );
+    initial begin
+        rst = 1;
+        instr <=0;
+        
+        #20
+        
+        // Vector Load, Store, Fused MAC (VMAC), vsetivli
+        // Scalar LW, LUI, ADDI, BEQ --> https://github.com/riscv/riscv-isa-manual/releases/download/Ratified-IMAFDQC/riscv-spec-20191213.pdf   page 130
+        
+        rst = 0;
+        // VSETAVLI              rd        uimm  zimm
+        #5  instr <= 32'b1010111_11110_111_00001_0011001100_1_1;
+        // VALU                  vd        vs1   vs2
+        #10 instr <= 32'b1010111_11101_000_00010_00011_0_101100;
+        // Vector Load           vd        rs1   
+        #10 instr <= 32'b0000111_11011_000_00100_00000_0_00_0_000;
+        // Vector Store          vd        rs1
+        #10 instr <= 32'b0100111_10111_000_01000_00000_0_00_0_000;
+        // Stream Out            ???
+        #10 instr <= 32'b1111111_01111_000_10000_00000_0000000;
+        
+        // LW                    rd        rs1
+        #10 instr <= 32'b1100000_00001_010_11110_001100110011;
+        // LUI                   rd
+        #10 instr <= 32'b1110110_00010_00110011001100110011;
+        // ADDI                  rd        rs1
+        #10 instr <= 32'b1100100_00100_000_11011_110011001100;
+        // BEQ                   imm?      rd1   rs2   imm again
+        #10 instr <= 32'b1100011_11001_000_10111_01000_0011001;
+        #20
+        
+        
+        
+        
+        
+        $finish;
+    end
+
 endmodule
--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_auto_incr_vec.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_auto_incr_vec.sv
@@ -6,6 +6,7 @@
 `endif
 
 module test_auto_incr_vec;
+    // dwidth_RFadd = 12;
     reg clk;
     reg rst;
     reg [dwidth_RFadd-1:0] ITR;
@@ -15,9 +16,9 @@ module test_auto_incr_vec;
     reg [dwidth_RFadd-1:0] vr_addr;
     reg [dwidth_RFadd-1:0] vw_addr;
     reg is_vmacc_vv;
-    reg is_streamout;
-    reg is_vle32_v;
-    reg is_vse32_v;
+    reg is_vstreamout;
+    reg is_vle32_vv;
+    reg is_vse32_vv;
     
     wire [dwidth_RFadd-1:0] vr_addr_auto_incr;
     wire [dwidth_RFadd-1:0] vw_addr_auto_incr;
@@ -28,8 +29,8 @@ module test_auto_incr_vec;
          .rst(rst),
          .ITR(ITR),
          .wen_ITR(wen_ITR),
-         .stall_rd(stall_rd_autovect), // clk_en
-         .stall_wr(stall_wr_autovect),
+         .stall_rd(stall_rd), // clk_en
+         .stall_wr(stall_wr),
          .is_vmacc_vv(is_vmacc_vv),
          .is_vle32_v(is_vle32_vv),
          .is_vse32_v(is_vse32_vv),
@@ -38,7 +39,7 @@ module test_auto_incr_vec;
          .vw_addr(vw_addr),
          .vr_addr_auto_incr(vr_addr_auto_incr),
          .vw_addr_auto_incr(vw_addr_auto_incr),
-         .done(done_auto_incr) // one clock pulse
+         .done(done) // one clock pulse
          );
     
     always begin
@@ -49,6 +50,47 @@ module test_auto_incr_vec;
     end
     
     initial begin
+        rst = 1; 
+        ITR <= 0;
+        wen_ITR = 0;
+        stall_rd = 0;
+        stall_wr = 0;
+        vr_addr <= 0;
+        vw_addr <= 0;
+        is_vmacc_vv = 0;
+        is_vle32_vv = 0;
+        is_vse32_vv = 0;
+        is_vstreamout = 0;
+        
+        
+        #20 rst = 0;
+        
+        // read
+        #5 wen_ITR = 1; #10 wen_ITR = 0;
+        vr_addr = 12'd8;
+        ITR = 8;
+        
+        #10 is_vmacc_vv = 1;
+        #20 is_vstreamout = 1;
+        #10 is_vmacc_vv = 0;
+        
+        #10 stall_rd = 1;
+        #40 stall_rd = 0;
+        #10 is_vse32_vv = 1;
+        #10 is_vstreamout = 0;
+        
+        //write
+        #60 wen_ITR = 1; #10 wen_ITR = 0;
+        vw_addr = 12'd32;
+        ITR = 8;
+        
+        #10 is_vmacc_vv = 1;
+        
+        #40 stall_wr = 1;
+        #10 is_vle32_vv = 1;
+        #20 is_vmacc_vv = 0;
+        #10 stall_wr = 0;
+        
         
         #100
     $finish;
