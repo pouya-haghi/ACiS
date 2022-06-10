427daaabe8a44ae743d74033f04f0115b305ef85
Krska <wkrska@caad-10k.bu.edu>
2022-06-10 12:05:41 -0400

    Test_vectorized_pe

--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_vectorized_PE.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_vectorized_PE.sv
@@ -42,62 +42,64 @@ module test_vectorized_PE;
     initial begin
         i1_PE_typeC <= 0;
         i2_PE_typeC <= 0;
-        i_tvalid1_PE_typeC <= 0;
-        i_tvalid2_PE_typeC <= 0;
+        i_tvalid1_PE_typeC <= {512{1'b0}};
+        i_tvalid2_PE_typeC <= {512{1'b0}};
         op <= 0;
         rst = 1;
         #20
         rst = 0;
         #15
-        i1_PE_typeC <= 512'h12345; i2_PE_typeC <= 512'h67890;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 000;
+        i1_PE_typeC <= 512'd12345; i2_PE_typeC <= 512'd67890;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 000;
         #10
-        i1_PE_typeC <= 512'h54321; i2_PE_typeC <= 512'h09876;
-        i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 1; op <= 000;
+        i1_PE_typeC <= 512'd54321; i2_PE_typeC <= 512'd09876;
+        i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 16'hFFFF; op <= 000;
         #10
         i1_PE_typeC <= 512'h10101; i2_PE_typeC <= 512'h12121;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 0; op <= 000;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 0; op <= 000;
         #10
-        i1_PE_typeC <= 512'h34343; i2_PE_typeC <= 512'h65656;
+        i1_PE_typeC <= 512'd34343; i2_PE_typeC <= 512'd65656;
         i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 0; op <= 000;
         
         #10 op <= 100;
         
         #10
-        i1_PE_typeC <= 512'h00001; i2_PE_typeC <= 512'h00004;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 001;
+        i1_PE_typeC <= 512'd00001; i2_PE_typeC <= 512'd00004;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 001;
         #10
-        i1_PE_typeC <= 512'h00010; i2_PE_typeC <= 512'h00040;
-        i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 1; op <= 001;
+        i1_PE_typeC <= 512'd00010; i2_PE_typeC <= 512'd00040;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 001;
         #10
-        i1_PE_typeC <= 512'h00100; i2_PE_typeC <= 512'h00400;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 0; op <= 001;
+        i1_PE_typeC <= 512'd00100; i2_PE_typeC <= 512'd00400;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 001;
         
         #10 op <= 100;
         
         #10
-        i1_PE_typeC <= 512'h77777; i2_PE_typeC <= 512'h00002;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 010;
+        i1_PE_typeC <= 512'd44444; i2_PE_typeC <= 512'd00002;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 010;
         #10
-        i1_PE_typeC <= 512'h55555; i2_PE_typeC <= 512'h00003;
-        i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 1; op <= 010;
+        i1_PE_typeC <= 512'd33333; i2_PE_typeC <= 512'd00003;
+        i_tvalid1_PE_typeC = 0; i_tvalid2_PE_typeC = 16'hFFFF; op <= 010;
         #10
-        i1_PE_typeC <= 512'h44444; i2_PE_typeC <= 512'h00004;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 0; op <= 010;
+        i1_PE_typeC <= 512'd22222; i2_PE_typeC <= 512'd00004;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 0; op <= 010;
         
         #10 op <= 100;
         
         #10
-        i1_PE_typeC <= 512'h00007; i2_PE_typeC <= 512'h00002;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 011;
+        i1_PE_typeC <= 512'd00005; i2_PE_typeC <= 512'd00002;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 011;
         #10
-        i1_PE_typeC <= 512'h00500; i2_PE_typeC <= 512'h00003;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 011;
+        i1_PE_typeC <= 512'd00300; i2_PE_typeC <= 512'd00003;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 011;
         #10
-        i1_PE_typeC <= 512'h40000; i2_PE_typeC <= 512'h00004;
-        i_tvalid1_PE_typeC = 1; i_tvalid2_PE_typeC = 1; op <= 011;
+        i1_PE_typeC <= 512'd20000; i2_PE_typeC <= 512'd00004;
+        i_tvalid1_PE_typeC = 16'hFFFF; i_tvalid2_PE_typeC = 16'hFFFF; op <= 011;
         
+        #10 i_tvalid1_PE_typeC = 16'h0; i_tvalid2_PE_typeC = 16'h0;
         
+        #200
         $finish;
     end
 endmodule
