18bea5bd1b3a1ecef1001c1c23eddd6b337b38a2
Krska <wkrska@caad-10k.bu.edu>
2022-06-14 09:56:58 -0400

    Changes to test_datapath

--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_datapath.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/test/test_datapath.sv
@@ -7,7 +7,7 @@
 
 module test_datapath;
     
-// reg [phit_size-1:0] inbound;                     
+    //////// DATAPATH ////////
     reg clk;
     reg rst;
     reg done_loader;
@@ -46,11 +46,29 @@ module test_datapath;
     wire [(dwidth_aximm*num_col)-1:0] awaddr_HBM;
     reg [num_col-1:0] awready_HBM;
            
- data_path data_path_inst1 (.*);
- 
-// localparam period = 5; 
-// localparam delay_HBM = 6;
- 
+    data_path data_path_inst1 (.*);
+    
+    //////// HBM ////////
+    hbm_0 hbm_0_inst_0 (
+        .SAXI_00(),
+        .SAXI_01(),
+        .SAPB_0(),
+        .HBM_REF_CLK_0(),
+        .AXI_00_ACLK(),
+        .AXI_00_ARESET_N(),
+        .AXI_00_WDATA_PARITY(),
+        .AXI_01_ACLK(),
+        .AXI_01_ARESET_N(),
+        .AXI_01_WDATA_PARITY(),
+        .APB_0_PCLK(),
+        .APB_0_PRESET_N(),
+        .AXI_00_RDATA_PARITY(),
+        .AXI_01_RDATA_PARITY(),
+        .apb_complete_0(),
+        .DRAM_0_STAT_CATTRIP(),
+        .DRAM_0_STAT_TEMP()
+        );
+    
  always begin
         clk = 1;
         #(clk_period/2);
@@ -65,7 +83,7 @@ module test_datapath;
     instr <= 0;
     rst = 0;
     
-    tdata_stream_in = 512'd3;
+    tdata_stream_in <= 512'd3;
     tvalid_stream_in <= {(SIMD_degree){1'b0}};
     tready_stream_out <= {(SIMD_degree){1'b0}};
     //
@@ -82,43 +100,43 @@ module test_datapath;
     rst = 1;
     #80;
     rst = 0;
-    #20;
+    #(clk_period*2);
     // vsetilvi
     // vlen = 8 = 12'b1000                    1,1         8       0     7     0     inst
-    instr[dwidth_inst-1:0] =               {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
-    instr[(2*dwidth_inst)-1:dwidth_inst] = {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
+    instr[dwidth_inst-1:0] <=               {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
+    instr[(2*dwidth_inst)-1:dwidth_inst] <= {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
     
-    #10;
+    #clk_period;
     // vle32
-    instr[dwidth_inst-1:0] =               {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h07}; // vle32.vv v0, (x2)
-    instr[(2*dwidth_inst)-1:dwidth_inst] = {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h07}; // vle32.vv v0, (x2)
+    instr[dwidth_inst-1:0] <=               {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h07}; // vle32.vv v0, (x2)
+    instr[(2*dwidth_inst)-1:dwidth_inst] <= {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h07}; // vle32.vv v0, (x2)
     arready_HBM <= {(num_col){1'b1}};
     #(clk_period*delay_HBM); // 8 cycles delay
     rvalid_HBM <= {(num_col){1'b1}};
     // stream in data
-    rdata_HBM <= 512'h1; #10;
-    rdata_HBM <= 512'h2; #10;
-    rdata_HBM <= 512'h3; #10;
-    rdata_HBM <= 512'h4; #10;
-    rdata_HBM <= 512'h5; #10;
-    rdata_HBM <= 512'h6; #10;
-    rdata_HBM <= 512'h7; #10;
-    rdata_HBM <= 512'h8; #10;
+    rdata_HBM <= 512'h1; #clk_period;
+    rdata_HBM <= 512'h2; #clk_period;
+    rdata_HBM <= 512'h3; #clk_period;
+    rdata_HBM <= 512'h4; #clk_period;
+    rdata_HBM <= 512'h5; #clk_period;
+    rdata_HBM <= 512'h6; #clk_period;
+    rdata_HBM <= 512'h7; #clk_period;
+    rdata_HBM <= 512'h8; #clk_period;
     rvalid_HBM <= {(num_col){1'b0}};
 
     // vsetilvi
     // vlen = 8 = 12'b1000                    1,1         8       0     7     0     inst
-    instr[dwidth_inst-1:0] =               {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
-    instr[(2*dwidth_inst)-1:dwidth_inst] = {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
+    instr[dwidth_inst-1:0] <=               {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
+    instr[(2*dwidth_inst)-1:dwidth_inst] <= {2'b11, 12'b1000, 3'b000, 3'h7, 5'b0, 7'h57}; // vsetivli x0, 0, e32, m2, 2048 
     
-    #10;
+    #clk_period;
     // vse32
-    instr[dwidth_inst-1:0] =               {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h27}; // vse32.vv v0, (x2)
-    instr[(2*dwidth_inst)-1:dwidth_inst] = {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h27}; // vse32.vv v0, (x2)
+    instr[dwidth_inst-1:0] <=               {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h27}; // vse32.vv v0, (x2)
+    instr[(2*dwidth_inst)-1:dwidth_inst] <= {12'b0, 5'b00010 , 3'b0, 5'h4, 7'h27}; // vse32.vv v0, (x2)
     awready_HBM <= {(num_col){1'b1}};
-    #80; // 8 cycles delay
+    #(clk_period*delay_HBM); // 8 cycles delay
     wready_HBM <= {(num_col){1'b1}};
-    #80; // read data for 8 cycles
+    #(clk_period*8); // read data for 8 cycles
     wready_HBM <= {(num_col){1'b0}};
     
 
