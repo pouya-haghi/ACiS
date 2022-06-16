8d9c9f0b9f478960c1e01d15c891ce7b84202c2c
pouya-haghi <pouya.haghi.hgh@gmail.com>
2022-06-16 16:07:20 -0400

    revert things back to one cycle latency for scalara instructions

--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/ISA_decoder.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/ISA_decoder.sv
@@ -29,7 +29,7 @@ module ISA_decoder(
     output logic [11:0] branch_immediate,
     output logic [dwidth_int-1:0] R_immediate,
     output logic [2:0] op,
-//    output logic [2:0] op_scalar,
+    output logic [2:0] op_scalar,
     output logic wen_RF_scalar
 //    output logic [dwidth_RFadd-1:0] VLEN_phy // to get the chunk size 
     // if it is v2 and VLEN_phy=32 then the correct base address is: 2*VLEN_phy
@@ -84,7 +84,7 @@ module ISA_decoder(
     
     assign R_immediate = (is_addi)? addi_immediate: lui_immediate;
     assign wen_RF_scalar = (is_addi || is_lui || is_csr || is_add)? 1'b1: 1'b0;
-//    assign op_scalar = (is_lui)? 3'b000: ((is_addi)?3'b001:(is_bne)?3'b010:(is_add)?3'b011:3'b100);
+    assign op_scalar = (is_lui)? 3'b000: ((is_addi)?3'b001:(is_bne)?3'b010:(is_add)?3'b011:3'b100);
     // ************************  vectorized instructions *************************
     assign op = (is_vmacc_vv)? 3'b011: 3'b100; //else: NoP 
     assign is_vect = |{is_vmacc_vv, is_vle32_vv, is_vse32_vv, is_vmv_vi, is_vstreamout};
--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/PE_scalar.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/PE_scalar.sv
@@ -9,32 +9,37 @@ module PE_scalar(
     input logic [dwidth_int-1:0] inp1,
     input logic [dwidth_int-1:0] inp2,
     input logic [dwidth_int-1:0] R_immediate,
-//    input logic [2:0] op_scalar,
-    input logic [dwidth_inst-1:0] instr_q,
-    input logic [1:0] is_forward_PE,
-    input logic clk,
-    input logic rst,
+    input logic [2:0] op_scalar,
+//    input logic [dwidth_inst-1:0] instr_q,
+//    input logic [1:0] is_forward_PE,
+//    input logic clk,
+//    input logic rst,
     output logic [dwidth_int-1:0] out1,
     output logic flag_neq
     );
     
-//    localparam [2:0] is_lui = 3'b000, is_addi = 3'b001, is_bne = 3'b010, is_add = 3'b011; 
-    logic is_addi_q, is_add_q, is_bne_q;
-    logic [dwidth_int-1:0] R_immediate_q;
-    logic [dwidth_int-1:0] out1_q;
+    localparam [2:0] is_lui = 3'b000, is_addi = 3'b001, is_bne = 3'b010, is_add = 3'b011; 
+//    logic is_addi_q, is_add_q, is_bne_q;
+//    logic [dwidth_int-1:0] R_immediate_q;
+//    logic [dwidth_int-1:0] out1_q;
     
-    assign is_addi_q = (instr_q[6:0]==7'b0010011 && instr_q[14:12]==3'b000)?1'b1:1'b0;
-    assign is_add_q = (instr_q[6:0]==7'b0110011 && instr_q[14:12]==3'b000 && instr_q[31:25]==7'b0000000)?1'b1:1'b0;
-    assign is_bne_q = (instr_q[6:0]==7'b1100011 && instr_q[14:12]==3'b001)?1'b1:1'b0;
+//    assign is_addi_q = (instr_q[6:0]==7'b0010011 && instr_q[14:12]==3'b000)?1'b1:1'b0;
+//    assign is_add_q = (instr_q[6:0]==7'b0110011 && instr_q[14:12]==3'b000 && instr_q[31:25]==7'b0000000)?1'b1:1'b0;
+//    assign is_bne_q = (instr_q[6:0]==7'b1100011 && instr_q[14:12]==3'b001)?1'b1:1'b0;
     
-    reg_r #(dwidth_int) reg_r_inst0 (R_immediate, clk, rst, R_immediate_q);// delay R_immediate for 1 cycle b/c addi is not a single-cycle instr (we should first read RF)
+//    reg_r #(dwidth_int) reg_r_inst0 (R_immediate, clk, rst, R_immediate_q);// delay R_immediate for 1 cycle b/c addi is not a single-cycle instr (we should first read RF)
     
-    assign out1 = (is_addi_q)? ((is_forward_PE[0])? out1_q+R_immediate_q : inp1+R_immediate_q): 
-    ((is_add_q)?((is_forward_PE[0])? out1_q+inp2: (is_forward_PE[1]? inp1+out1_q : inp1+inp2)) : 
+//    assign out1 = (is_addi_q)? ((is_forward_PE[0])? out1_q+R_immediate_q : inp1+R_immediate_q): 
+//    ((is_add_q)?((is_forward_PE[0])? out1_q+inp2: (is_forward_PE[1]? inp1+out1_q : inp1+inp2)) : 
+//    {(dwidth_int){1'b0}});
+    
+    assign out1 = (op_scalar==is_addi)? inp1+R_immediate: 
+    ((op_scalar==is_add)? inp1+inp2 : 
     {(dwidth_int){1'b0}});
     
-    assign flag_neq = ((inp1 != inp2) & (is_bne_q))? 1'b1: 1'b0;
+//    assign flag_neq = ((inp1 != inp2) & (is_bne_q))? 1'b1: 1'b0;
+    assign flag_neq = ((inp1 != inp2) & (op_scalar==is_bne))? 1'b1: 1'b0;
     
-    reg_r #(dwidth_int) reg_r_inst1 (out1, clk, rst, out1_q); // used for forwarding
+//    reg_r #(dwidth_int) reg_r_inst1 (out1, clk, rst, out1_q); // used for forwarding
     
 endmodule
