c80c4b12bbde52f4bee98066e59506cbe50fe376
Pouya Haghi <pouya@nat-wireless-guest-reg-153-50.bu.edu>
2022-07-07 11:14:24 -0400

    corrected the logic for wr_en in rtl_kernel_wizard_0_runTimeLoadTable.sv because wr_en has to be asserted at the same clock when rxfer becomes high (we had one clock dealy mistake for wr_en in the prev version)

--- NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/rtl_kernel_wizard_0_runtimeLoadTable.sv
+++ NetFPGA-PLUS/hw/lib/std/CGRA_v1_0_0/src/rtl_kernel_wizard_0_runtimeLoadTable.sv
@@ -303,9 +303,9 @@ inst_ar_to_r_transaction_cntr (
 // assign m_axis_tlast  = m_axi_rlast;
 
   localparam [dwidth_configadd-1:0] num_entry_config_table = depth_config;
-  localparam [1:0] config_state0 = 2'b00, config_state1 = 2'b01, config_state2 =2'b10, config_state3 = 2'b11;
-  logic [1:0] config_state;
-  logic transition_signal;
+  localparam [1:0] init = 2'b00, first_sixteen_column = 2'b01, second_sixteen_column =2'b10;
+  logic [1:0] config_state, congig_state_next;
+  // logic transition_signal;
   logic [dwidth_configadd-1:0] wr_add; // for state_table and config table
   logic [num_col-1:0] wr_en; // for state_table and config tables
 
@@ -371,35 +371,40 @@ inst_ar_to_r_transaction_cntr (
     end
     
     // generating wr_en for selecting the correct configuration/state tables
-    assign transition_signal = (wr_en == '0 && rxfer)? 1'b1: 1'b0;
+    // assign transition_signal = (wr_en == '0 && rxfer)? 1'b1: 1'b0;
     always_ff @(posedge aclk) begin
         if (areset)
-//            wr_en <= 0;
-            config_state <= config_state0;
-        else begin
-            if (transition_signal)
-//                wr_en <= {{(num_col-(phit_size/dwidth_int)){1'b0}}, {(phit_size/dwidth_int){1'b1}}}; // 16 '0's and 16 '1's. enable 16 config tables at the same time
-                config_state <= config_state1;
-                // This is b/c axi is wide (512 bits) and it can support multiple parallel tables
-            else if (wr_en != 0 && wr_add == t_num_entry_config_table && rxfer) //num_entry_config_table - 1
-//                wr_en <= wr_en << (phit_size/dwidth_int);
-                config_state <= config_state2;
-            else if (done)// done state, m_axi_rlast
-//                wr_en <= 0; //avoid keeping wr_en high
-                config_state <= config_state3;
-        end
-    end
-    
-    always_comb begin
-        if (config_state == config_state0 && !transition_signal)
-            wr_en = '0;
-        else if ((config_state == config_state0 && transition_signal) | config_state == config_state1)
-            wr_en = {{(num_col-(phit_size/dwidth_int)){1'b0}}, {(phit_size/dwidth_int){1'b1}}}; // 16 '0's and 16 '1's. enable 16 config tables at the same time
-        else if (config_state == config_state2)
-            wr_en = wr_en << (phit_size/dwidth_int);
+            config_state <= init;
         else
-            wr_en = '0;
+            config_state <= config_state_next;
     end
+
+    always_ff @(posedge aclk) begin 
+        case(config_state)
+            init: config_state_next <= (rxfer) ? first_sixteen_column : init;
+            // first_sixteen_column: // wr_en is  {num_col-16} '0's and 16 '1's. enable 16 config tables at the same time (if num_col<16 verilog doesnt make a bug)
+            first_sixteen_column: config_state_next <= (wr_add == t_num_entry_config_table && rxfer)? second_sixteen_column : first_sixteen_column;
+            // second_sixteen_column: // wr_en is  {num_col-16} '1's and 16 '0's. enable 16 config tables at the same time
+            second_sixteen_column: config_state_next <= (done)? init: second_sixteen_column;
+            default: config_state_next <= init
+        endcase
+    end
+
+    assign wr_en = ((config_state == init && rxfer)|(config_state == first_sixteen_column)) ? {{(num_col-(phit_size/dwidth_int)){1'b0}}, {(phit_size/dwidth_int){1'b1}}} : ((config_state == second_sixteen_column) ? {{(num_col-(phit_size/dwidth_int)){1'b1}}, {(phit_size/dwidth_int){1'b0}}} : 'b0);
+
+//     always_ff @(posedge aclk) begin
+//         if (areset)
+// //            wr_en <= 0;
+//         else begin
+//             if (transition_signal)
+// //                wr_en <= {{(num_col-(phit_size/dwidth_int)){1'b0}}, {(phit_size/dwidth_int){1'b1}}}; // 16 '0's and 16 '1's. enable 16 config tables at the same time
+//                 // This is b/c axi is wide (512 bits) and it can support multiple parallel tables
+//             else if (wr_en != 0 && wr_add == t_num_entry_config_table && rxfer) //num_entry_config_table - 1
+// //                wr_en <= wr_en << (phit_size/dwidth_int);
+//             else if (done)// done state, m_axi_rlast
+// //                wr_en <= 0; //avoid keeping wr_en high
+//         end
+//     end
 
 // end
 // PH
