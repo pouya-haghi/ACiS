37b933e1dc81efc26b8ba1571fefda2ad100d014
pouya-haghi <pouya.haghi.hgh@gmail.com>
2022-08-03 17:21:01 -0400

    [updated] GCN_results.ipynb and [added] GCN_results folder containing data

---
+++ GCN_results/constant.py
@@ -0,0 +1,11 @@
+HEADER_LENGTH= 34 # in Bytes
+PHIT_SIZE = 64 # Bytes
+# While NetFPGA runs at 3 ns lets pick 4 ns for our design
+PERIOD_CLK = 4 # ns (250 MHz)
+# PERIOD_CLK = 3 # ns (333.33 MHz)
+SWITCH_LATENCY = 171 # ns
+PE_LATENCY = 8 # cycles
+NUM_COL = 16
+SIMD_DEGREE = 16
+# NUM_EXPERIMENTS = 1000
+# SWEEP_STEP = 500 # ns
