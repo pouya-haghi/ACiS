a48b227510c554d0d3d2e48e0536436e0adfe23a
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-01 20:22:36 -0400

    optimizations

--- MPI/host_cfg.py
+++ MPI/host_cfg.py
@@ -11,44 +11,16 @@ def setup_host(rank_list: list,  alveo_port: int, size: int, xclbin_path='/binar
 
     currentDevice = pynq.Device.devices[0]
     ol = pynq.Overlay(xclbin_path, device=currentDevice)
-    print(ol.ip_dict)
+    # print(ol.ip_dict)
 
     print(f'Link interface 0 {ol.cmac_0.link_status()}')
 
     # THE FOLLOWING IP ADDRESS SHOULD BE IN THE SAME SUBNET
-    print(ol.networklayer_0.set_ip_address(alveo_ipaddr, debug=True))
+    ol.networklayer_0.set_ip_address(alveo_ipaddr, debug=True)
 
     # Execute ifconfig command
-    ifconfig_output = subprocess.check_output(["ifconfig", "enp175s0"]).decode("utf-8")
-    print(ifconfig_output)
-
-    # Execute ping command
-    # max_attempts = 5
-    # attempts = 0
-
-    # while attempts < max_attempts:
-    #     try:
-    #         # Execute ping command
-    #         ping_output = subprocess.run(["ping","-c", "5", alveo_ipaddr], capture_output=True, text=True)
-
-    #         if ping_output.returncode == 0:
-    #             print(ping_output.stdout)
-    #             break
-    #         else:
-    #             attempts += 1
-    #             print(f"Attempt {attempts}: Ping command failed with return code {ping_output.returncode}")
-    #             print(ping_output.stderr)
-
-    #     except subprocess.CalledProcessError as err:
-    #         attempts += 1
-    #         print(f"Attempt {attempts}: Error executing ping command: {str(err)}")
-
-    #     # Sleep for a few seconds before the next attempt
-    #     time.sleep(1)
-
-    # if attempts == max_attempts:
-    #     print(f"Failed to reach the target IP after {max_attempts} attempts. Exiting.")
-    #     sys.exit(1)
+    # ifconfig_output = subprocess.check_output(["ifconfig", "enp175s0"]).decode("utf-8")
+    # print(ifconfig_output)
 
     ol.networklayer_0.arp_discovery()
 
--- MPI/node_ctrl.py
+++ MPI/node_ctrl.py
@@ -1,25 +1,9 @@
-import subprocess
 import multiprocessing
 import sys
 import json
 import logging
 import time
 import node_exec as exec
-
-def ping_fpga(alveo_ip):
-    # Ping FPGA
-    max_attempts = 5
-    logging.debug(f"Establishing connection to FPGA at: {alveo_ip}")
-    for attempt in range(1, max_attempts + 1):
-        try:
-            # Execute ping command
-            subprocess.run(["ping", "-c", "1", alveo_ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            logging.debug(f"Successfully pinged {alveo_ip}")
-            return  # Exit the function if the ping is successful
-        except subprocess.CalledProcessError as err:
-            logging.debug(f"Ping attempt {attempt}/{max_attempts} failed: {str(err)}")
-
-    raise Exception("Failed to ping the destination after 5 attempts. Exiting program.")
 
 def execute_port(alveo_ip, alveo_port, port, size):
     exec.execute(alveo_ip, alveo_port, port, size)
@@ -40,8 +24,6 @@ if __name__ == "__main__":
         logging.debug(f"\nInput: alveo_ip={alveo_ip}\nalveo_port={alveo_port}\nsize={size}\nport_list={port_list}")
         
         start_time = time.time()
-
-        # ping_fpga(alveo_ip=alveo_ip)
 
         fpga_time = time.time()
 
