bb8ba657d02fd17c3cc1826e58263ada8f68e437
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-04 11:48:36 -0400

    Update node_exec.py

--- MPI/IntAllReduce/node_exec.py
+++ MPI/IntAllReduce/node_exec.py
@@ -45,16 +45,20 @@ async def execute(alveo_ip: str, alveo_port: int, port_num: int, size: int):
             local_addr=('localhost', port_num))
 
         # Send packets and perform the sieve concurrently
-        tasks = [
-            send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts),
-            loop.run_in_executor(None, sieve_of_eratosthenes, 1500000)
-        ]
-        await asyncio.gather(*tasks)
+        send_task = send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts)
+        sieve_future = loop.run_in_executor(None, sieve_of_eratosthenes, 1500000)
+
+        # Await both tasks to complete
+        done, _ = await asyncio.wait([send_task, sieve_future])
+
+        # Get the result from the sieve_future
+        primes = sieve_future.result()
 
         np.savetxt(f'{port_num}_output.txt', udp_message_global, fmt='%d')
         np.savetxt(f'{port_num}_recv_data.txt', protocol.buffer, fmt='%d')
 
         transport.close()
     except Exception as err:
+        if transport is not None:
+            transport.close()
         raise Exception(f"Error! Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}")
-
