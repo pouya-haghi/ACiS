e776e6f58138e86dc498351a909e4dea65be22d8
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-04 16:30:49 -0400

    Update node_exec.py

--- MPI/IntAllReduce/node_exec.py
+++ MPI/IntAllReduce/node_exec.py
@@ -1,8 +1,8 @@
 # asynio test
-
-import asyncio
-import numpy as np
+import threading
+import socket
 import logging
+import numpy as np
 
 BYTES_PER_PACKET = 1408
 
@@ -16,61 +16,61 @@ def sieve_of_eratosthenes(n):
                 sieve[i] = False
     return primes
 
-class DatagramProtocol(asyncio.DatagramProtocol):
-    def __init__(self, data, size):
-        self.data = data
-        self.size = size
-        self.buffer = np.empty((size, 1), dtype=np.uint8)
-        self.transport = None
-        self.send_complete = asyncio.Event()  # Event to signal completion of sending
 
-    def connection_made(self, transport):
-        self.transport = transport
+def socket_receive_threaded(sock, size, port_num):
+    try:    
+        shape_global = (size,1)
+        shape_local = (BYTES_PER_PACKET,1)
+        recv_data_global = np.empty(shape_global, dtype = np.uint8)
+        data_partial = np.empty(shape_local, dtype = np.uint8)
+        num_it = (size // BYTES_PER_PACKET)
+        sum_bytes = 0
+        connection = 'None'
+        for m in range(num_it):
+            res = sock.recvfrom_into(data_partial)
+            recv_data_global[(m * BYTES_PER_PACKET) : ((m * BYTES_PER_PACKET) \
+                            + BYTES_PER_PACKET)] = data_partial
+            sum_bytes = sum_bytes + int(res[0])
+            connection = res[1]
+        np.savetxt(f'{port_num}_recv_data.txt', recv_data_global, fmt='%d')
+    except Exception as err:
+        raise Exception(f"Could not complete socket_recieve_threaded() with socket {sock}! Error: {str(err)}")
 
-    def datagram_received(self, data, addr):
-        # wait for the send operation to complete before processing received data
-        if self.send_complete.is_set():
-            data = np.frombuffer(data, dtype=np.uint8).reshape(-1, 1)
-            logging.debug(f'Received data: {data}')
-            self.buffer[:data.shape[0]] = data
 
-async def send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts):
-    for m in range(num_pkts):
-        udp_message_local = udp_message_global[
-            (m * BYTES_PER_PACKET):((m * BYTES_PER_PACKET) + BYTES_PER_PACKET)
-        ]
-        protocol.transport.sendto(udp_message_local.tobytes(), (alveo_ip, alveo_port))
-        logging.debug(f"Sent packet: {udp_message_local}")
-    protocol.send_complete.set() 
-
-async def execute(alveo_ip: str, alveo_port: int, port_num: int, size: int):
-    logging.debug("Beginning execute.")
-
+def socket_send_threaded(sock, udp_message_global, alveo_ip, alveo_port, size):
     try:
-        loop = asyncio.get_running_loop()
+        num_pkts = size // BYTES_PER_PACKET
+        for m in range(num_pkts):
+            udp_message_local = udp_message_global[(m * BYTES_PER_PACKET):((m * BYTES_PER_PACKET) + BYTES_PER_PACKET)]
+            sock.sendto(udp_message_local, (alveo_ip, alveo_port))
+    except Exception as err:
+        raise Exception(f"Could not complete socket_send_threaded() with socket {sock}! Error: {str(err)}")
+
+
+def execute(alveo_ip: str, alveo_port: int, port_num: int, size: int, sieve_size: int):
+    try:
+        logging.debug(f'Starting execute for {port_num}')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        sock.bind(('', port_num))
 
         shape = (size, 1)
         udp_message_global = np.random.randint(low=0, high=((2 ** 8) - 1), size=shape, dtype=np.uint8)
-        num_pkts = size // BYTES_PER_PACKET
 
-        logging.debug('Before create_datagram_endpoint')
-        transport, protocol = await loop.create_datagram_endpoint(
-            lambda: DatagramProtocol(udp_message_global, size),
-            local_addr=('localhost', port_num))
-        logging.debug('After create_datagram_endpoint')
+        recv_thread = threading.Thread(target=socket_receive_threaded, args=(sock, size,port_num,))
+        send_thread = threading.Thread(target=socket_send_threaded, args=(sock, udp_message_global, alveo_ip, alveo_port, size,))
+        sieve_thread = threading.Thread(target=sieve_of_eratosthenes, args=(sieve_size,))
 
-        logging.debug('Before send_packets')
-        # run send_packets and sieve_of_eratosthenes concurrently
-        await asyncio.gather(
-            send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts),
-            asyncio.to_thread(sieve_of_eratosthenes, 1500000))
-        logging.debug('After send_packets')
+        # Start the threads
+        recv_thread.start()
+        send_thread.start()
+        sieve_thread.start()
 
-        np.savetxt(f'{port_num}_output.txt', udp_message_global, fmt='%d')
-        np.savetxt(f'{port_num}_recv_data.txt', protocol.buffer, fmt='%d')
+        # Wait for all threads to finish
+        recv_thread.join()
+        send_thread.join()
+        sieve_thread.join()
+
     except Exception as err:
-        logging.exception(f"Error! Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}", exc_info=True)
-        raise Exception(f"Error! Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}")
+        raise Exception(f"Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}")
     finally:
-        if transport:
-            transport.close()
+        sock.close()
