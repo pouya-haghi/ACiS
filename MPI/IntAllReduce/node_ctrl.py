9d95fb3bd427ae3e6bdebc890d938a0450573ab0
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-04 17:09:36 -0400

    Update node_ctrl.py

--- MPI/IntAllReduce/node_ctrl.py
+++ MPI/IntAllReduce/node_ctrl.py
@@ -1,33 +1,20 @@
+import multiprocessing
 import sys
 import json
 import logging
 import time
 import node_exec as exec
-import asyncio
 
-async def main(alveo_ip, alveo_port, port_list, size):
-    tasks = [asyncio.create_task(execute_port_async(alveo_ip, alveo_port, port, size)) for port in port_list]
-    await asyncio.gather(*tasks)
-
-async def execute_port_async(alveo_ip, alveo_port, port, size):
-    try:
-        await exec.execute(alveo_ip, alveo_port, port, size)
-    except Exception as e:
-        logging.exception('Exception in execute_port_async', exc_info=True)
+def execute_port(alveo_ip, alveo_port, port, size):
+    exec.execute(alveo_ip, alveo_port, port, size)
 
 if __name__ == "__main__":
     try:
-        start_time = time.time()
-
-        # set up logging
+        start_time = time.time()    
         logging.basicConfig(filename='node_ctrl.log', level=logging.DEBUG,
                             format='%(asctime)s - %(levelname)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
-        # add a StreamHandler to see logs in console
-        logging.getLogger().addHandler(logging.StreamHandler())
-        
         logging.debug("Beginning node_ctrl.py.")
-        
         # Get args
         alveo_ip = sys.argv[1]
         alveo_port = int(sys.argv[2])
@@ -36,13 +23,16 @@ if __name__ == "__main__":
         port_list = json.loads(port_json)
 
         logging.debug(f"\nInput: alveo_ip={alveo_ip}\nalveo_port={alveo_port}\nsize={size}\nport_list={port_list}")
-
-        exec_time = time.time()
         
-        logging.debug('About to create tasks...')
-        asyncio.run(main(alveo_ip, alveo_port, port_list, size))
+        exec_time = time.time()
+
+        with multiprocessing.Pool() as pool:
+            args_list = [(alveo_ip, alveo_port, port, size) for port in port_list]
+            pool.starmap(execute_port, args_list)
+
         end_time = time.time()
 
-        logging.debug(f"Async execution complete in node_ctrl.py.\nTotal Elapsed time = {end_time-start_time}\nExecution time = {exec_time-end_time}")
+        logging.debug(f"Multiprocessing complete in node_ctrl.py.\nTotal Elapsed time = {end_time-start_time}\nExecution time = {exec_time-end_time}")
     except Exception as err:
-        logging.exception(f"Failed! Did not complete execution! Error: {str(err)}", exc_info=True)
+        logging.debug(f"Failed! Did not complete execution! Error: {(str(err))}")
+
