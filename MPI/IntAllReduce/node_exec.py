cf18d694f240019b8b8a27ba45d15387d7f65482
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-04 15:00:45 -0400

    Update node_exec.py

--- MPI/IntAllReduce/node_exec.py
+++ MPI/IntAllReduce/node_exec.py
@@ -1,7 +1,8 @@
+# asynio test
+
 import asyncio
 import numpy as np
 import logging
-
 
 BYTES_PER_PACKET = 1408
 
@@ -22,48 +23,29 @@ class DatagramProtocol(asyncio.DatagramProtocol):
         self.buffer = np.empty((size, 1), dtype=np.uint8)
         self.transport = None
         self.send_complete = asyncio.Event()  # Event to signal completion of sending
-        self.received_queue = asyncio.Queue()  # asyncio.Queue to store received data
 
     def connection_made(self, transport):
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        # when a datagram is received, put it into the queue
-        self.received_queue.put_nowait(data)
+        # wait for the send operation to complete before processing received data
+        if self.send_complete.is_set():
+            data = np.frombuffer(data, dtype=np.uint8).reshape(-1, 1)
+            logging.debug(f'Received data: {data}')
+            self.buffer[:data.shape[0]] = data
 
-
-async def socket_receive_async(protocol, size):
-    try:
-        shape_global = (size, 1)
-        shape_local = (BYTES_PER_PACKET, 1)
-        recv_data_global = np.empty(shape_global, dtype=np.uint8)
-        data_partial = np.empty(shape_local, dtype=np.uint8)
-        num_it = (size // BYTES_PER_PACKET)
-        sum_bytes = 0
-        connection = 'None'
-
-        for m in range(num_it):
-            data_partial = await protocol.received_queue.get()
-            recv_data_global[(m * BYTES_PER_PACKET):((m * BYTES_PER_PACKET) + BYTES_PER_PACKET)] = np.frombuffer(data_partial, dtype=np.uint8)
-            sum_bytes += len(data_partial)
-
-        return recv_data_global
-
-    except Exception as err:
-        raise Exception(f"Could not complete socket_receive_async()! Error: {str(err)}")
-
-
-async def send_packets_async(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts):
+async def send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts):
     for m in range(num_pkts):
         udp_message_local = udp_message_global[
             (m * BYTES_PER_PACKET):((m * BYTES_PER_PACKET) + BYTES_PER_PACKET)
         ]
         protocol.transport.sendto(udp_message_local.tobytes(), (alveo_ip, alveo_port))
-    protocol.send_complete.set()  # set the event after send operation completes
-
+        logging.debug(f"Sent packet: {udp_message_local}")
+    protocol.send_complete.set() 
 
 async def execute(alveo_ip: str, alveo_port: int, port_num: int, size: int):
     logging.debug("Beginning execute.")
+
     try:
         loop = asyncio.get_running_loop()
 
@@ -71,22 +53,21 @@ async def execute(alveo_ip: str, alveo_port: int, port_num: int, size: int):
         udp_message_global = np.random.randint(low=0, high=((2 ** 8) - 1), size=shape, dtype=np.uint8)
         num_pkts = size // BYTES_PER_PACKET
 
+        logging.debug('Before create_datagram_endpoint')
         transport, protocol = await loop.create_datagram_endpoint(
             lambda: DatagramProtocol(udp_message_global, size),
             local_addr=('localhost', port_num))
+        logging.debug('After create_datagram_endpoint')
 
-        # Run the tasks concurrently using asyncio.gather
-        recv_task = socket_receive_async(protocol, size)
-        send_task = send_packets_async(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts)
-
+        logging.debug('Before send_packets')
+        # run send_packets and sieve_of_eratosthenes concurrently
         await asyncio.gather(
-            recv_task,
-            send_task,
+            send_packets(protocol, udp_message_global, alveo_ip, alveo_port, num_pkts),
             asyncio.to_thread(sieve_of_eratosthenes, 1500000))
+        logging.debug('After send_packets')
 
         np.savetxt(f'{port_num}_output.txt', udp_message_global, fmt='%d')
-        np.savetxt(f'{port_num}_recv_data.txt', recv_task.result(), fmt='%d')  # Save the received data
-
+        np.savetxt(f'{port_num}_recv_data.txt', protocol.buffer, fmt='%d')
     except Exception as err:
         logging.exception(f"Error! Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}", exc_info=True)
         raise Exception(f"Error! Could not complete execute() on {alveo_ip}:{alveo_port}! Error: {str(err)}")
