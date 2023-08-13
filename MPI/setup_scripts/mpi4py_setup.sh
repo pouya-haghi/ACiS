5e53456f653f4f849a36d2bac48721161f91fdcb
Ian Chadwick <ianjchadwick@gmail.com>
2023-08-13 16:47:08 -0400

    Update mpi4py_setup.sh

--- MPI/setup_scripts/mpi4py_setup.sh
+++ MPI/setup_scripts/mpi4py_setup.sh
@@ -18,5 +18,13 @@ pip3 install pynq==2.8.0.dev0
 # Make sure openssh-sever is installed
 sudo apt-get install openssh-server
 
+# Create hostfile with hostnames of the remote leaf nodes involved
+nano hosts.txt
+
+# Ensure the python script is already on each of the nodes
+# Copy test.py to each node that will execute it.
+# e.g.
+nano test.py
+
 #ssh into each computer from each computer before this step
 mpiexec -n 2 --hostfile hosts.txt --env MPIEXEC_SSHIDENT=~/.ssh/key python3 test.py
